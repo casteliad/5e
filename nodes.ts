c01c2f85962d0804a1761ada0aa07c17e2a74a8a
casteliad <56516455+casteliad@users.noreply.github.com>
2023-10-02 16:16:45 +0200

    Update nodes.ts

--- nodes.ts
+++ nodes.ts
@@ -1,10 +1,24 @@
-import { Node } from "reactflow";
-
-export default [
+[
   {
-    id: "1",
-    type: "input",
-    data: { label: "Input" },
-    position: { x: 250, y: 25 }
+    "id": "node-1",
+    "type": "input",
+    "data": {
+      "label": "Input Node"
+    },
+    "position": {
+      "x": 100,
+      "y": 100
+    }
+  },
+  {
+    "id": "node-2",
+    "type": "output",
+    "data": {
+      "label": "Output Node"
+    },
+    "position": {
+      "x": 300,
+      "y": 100
+    }
   }
-] as Node[];
+]
